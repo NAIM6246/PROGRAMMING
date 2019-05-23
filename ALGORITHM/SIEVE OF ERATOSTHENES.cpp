9939a08653495cc139709b9011e207a31d02174c
NAIM6246 <36632723+NAIM6246@users.noreply.github.com>
2019-05-23 06:15:56 +0600

    Add files via upload

---
+++ ALGORITHM/SIEVE OF ERATOSTHENES.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+    bool prime[1000005];
+void SieveOfEratosthenes(int n)
+{
+    memset(prime,true,n);
+    for(int i=2; i*i<=n; i++)
+    {
+        if(prime[i]==true)
+        {
+            for(int j=i*2; j<=n; j+=i)
+            {
+                prime[j]=false;
+            }
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    SieveOfEratosthenes(n);
+    for(int i=2;i<=n;i++){
+        if(prime[i])
+            cout<<i<<endl;
+    }
+}
