84adfa1bed5487395e160194509482599989c32a
NAIM6246 <36632723+NAIM6246@users.noreply.github.com>
2019-05-24 00:24:46 +0600

    Create SEIVE OF ERASTOTHENES(MODIFIED).cpp
    modified sieve

---
+++ ALGORITHM/SEIVE OF ERASTOTHENES(MODIFIED).cpp
@@ -0,0 +1,34 @@
+/*
+    sieve of erastosthenes modified and efficient
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+bool prime[1000005];
+void SieveOfEratosthenes(int n)
+{
+    memset(prime,true,n);
+    for(int i=4;i<=n;i+=2)
+        prime[i]=0;
+    for(int i=3; i*i<=n; i++)
+    {
+        if(prime[i]==true)
+        {
+            for(int j=i*i; j<=n; j+=(i+i))
+            {
+                prime[j]=false;
+            }
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    SieveOfEratosthenes(n);
+    for(int i=2; i<=n; i++)
+    {
+        if(prime[i])
+            cout<<i<<endl;
+    }
+}
