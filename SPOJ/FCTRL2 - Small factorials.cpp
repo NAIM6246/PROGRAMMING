d7168e8aff690ba865e01fd990a27af4f7f038c2
NAIM6246 <36632723+NAIM6246@users.noreply.github.com>
2019-05-27 00:26:52 +0600

    Create FCTRL2 - Small factorials.cpp

---
+++ SPOJ/FCTRL2 - Small factorials.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int>res;
+void factorial(long long x)
+{
+    long long carry=0,prod;
+    for(long long j=0; j<res.size(); j++)
+    {
+        prod = res[j]*x + carry;
+        res[j] = prod%10;
+        carry=prod/10;
+    }
+    while(carry>0)
+    {
+        res.push_back(carry%10);
+        carry=carry/10;
+    }
+}
+
+
+int main()
+{
+    long long t;
+    cin>>t;
+    while(t--){
+    long long n;
+    cin>>n;
+    res.push_back(1);
+    for(long long i=2; i<=n; i++)
+    {
+        factorial(i);
+    }
+    for(long long i=res.size()-1; i>=0; i--)
+        printf("%d",res[i]);
+    printf("\n");
+    res.clear();
+    }
+    return 0;
+}
